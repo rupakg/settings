e859efeba0475e84813c09c8e8c433ac6111fec0
Rupak Ganguly <rupakg@gmail.com>
2011-07-23 01:20:49 -0400

    Add a generic alias file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+alias p="cd ~/projects"
+alias u="unicorn -c unicorn.conf.rb"
+alias cw="compass watch"
+
+# Titanium stuff
+alias titanium='/Library/Application\ Support/Titanium/mobilesdk/osx/1.6.1/titanium.py'
+alias ti-build='/Library/Application\ Support/Titanium/mobilesdk/osx/1.6.1/iphone/builder.py'
+alias tib='ti-build run . 4.3'
+alias tib2='ti-build run . 4.3 ipad'
+alias coffee-t='coffee -c --bare -o Resources --watch src'
+
+# tmuxinator aliases
+alias tm='tmuxinator'
+
+# macosx stuff
+alias show_hf="defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder"
+alias hide_hf="defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder"
